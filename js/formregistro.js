e502c5e2081d52335e71e92c056388c8bea2963f
sofiamellano <mellanosofia1@gmail.com>
2023-09-13 17:30:11 -0300

    cambios de ultimo momento form-auth

--- js/formregistro.js
+++ js/formregistro.js
@@ -1,10 +1,11 @@
-import { auth } from "js/firebase.js";
+import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 
 
 const formRegristro=document.getElementById("registroForm")
 
-formRegristro.addEventListener("submit",async ()=>{
+formRegristro.addEventListener("submit",async (e)=>{
+    e.preventDefault();
     const email=formRegristro['txtEmail'].value;
     const password=formRegristro['txtPassword'].value;
     await createUserWithEmailAndPassword(auth, email, password);
