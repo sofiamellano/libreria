b0c3f3c2443dace52807fc832b78f51444e05c69
sofiamellano <mellanosofia1@gmail.com>
2023-09-27 17:32:40 -0300

    boton agregar y editar

--- js/app.js
+++ js/app.js
@@ -16,13 +16,19 @@ const applibros = {
                 contenidoHTML+= `
                 <div>
                     <img src="${libro.portada_url}" class="img-thumbnail"/>
+                    <h4>${libro.nombre}</h4>
+                    <h5>Autor: ${libro.autor}</h5>
+                    <h5>Genero: ${libro.genero}</h5>
                     <details>
-                    <summary>${libro.nombre}</summary>
-                    ${libro.sinopsis}
+                    <summary>Mas Info:</summary>
+                    Sinopsis: ${libro.sinopsis}<br/>
+                    Paginas: ${libro.paginas}<br/>
+                    Editorial: ${libro.editorial}
                     </details>
+                    <a href="#" onclick="applibros.editarLibro('${libro._id}')">Editar</a>
+
                 </div>
                 `
-                
             }
             console.log(contenidoHTML)
             contenedor.innerHTML=contenidoHTML;
@@ -31,7 +37,44 @@ const applibros = {
         
     },
     agregarLibros:()=>{
-        alert("agregando un libro");
+        const txtNombre=document.getElementById("txtNombre");
+        const txtPaginas=document.getElementById("txtPaginas");
+        const txtAutor=document.getElementById("txtAutor");
+        const txtEditorial=document.getElementById("txtEditorial");
+        const txtPortadaUrl=document.getElementById("txtPortadaUrl");
+        const txtSinopsis=document.getElementById("txtSinopsis");
+        const txtGenero=document.getElementById("txtGenero");
+        
+        const nuevoLibro = {
+            "nombre": txtNombre.value,
+            "paginas": txtPaginas.value,
+            "autor": txtAutor.value,
+            "editorial": txtEditorial.value,
+            "portada_url": txtPortadaUrl.value,
+            "sinopsis": txtSinopsis.value,
+            "genero": txtGenero.value,
+        };
+        console.log(nuevoLibro);
+        fetch(urlApi, {
+            method: "POST",
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(nuevoLibro)
+            })  
+            .then(response => {
+            console.log(response);
+            window.location.href="index.html";
+              //return movies.obtenerTodos();
+            });
+    },
+    editarLibro:(idLibroAEditar)=>{
+        const urlApi=`https://ingenieria-bd23.restdb.io/rest/libros/${idLibroAEditar}?apikey=64f8c70c68885409dc0bfe7f`;
+        fetch(urlApi).then(res => res.json()).then(libro =>{
+            document.getElementById("txtNombre").value=libro.nombre;
+            document.getElementById("txtPaginas").value=libro.paginas;
+            console.log(libro);
+        });
     }
 }
 applibros.listarLibros();
